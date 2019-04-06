b45d904a6be3f6d99aa495b2826d747803cdacb2
Bots.Business <git.sync@bots.business>
2019-04-06 19:58:32 +0000

    Bots.Business export for bot id: 15037

--- commands/6.js
+++ commands/6.js
@@ -1,10 +1,10 @@
 /*CMD
-  command: 5
+  command: 6
   help: 
   need_reply: 
   auto_retry_time: 
-  answer: Lista 5
-  keyboard: 4, BotInfo, beta, 6
+  answer: Lista 6
+  keyboard: 5, BJS
   aliases: 
 CMD*/
 
